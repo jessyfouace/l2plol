723c023c6a770102514b9bfefd5c2a40f1d79b10
jessyfouace <jessy.fouace62@gmail.com>
2020-11-13 04:30:52 +0100

    add light css with variable

--- views/template/header.php
+++ views/template/header.php
@@ -14,6 +14,7 @@
   <!-- Place favicon.ico in the root directory -->
 
   <link rel="stylesheet" href="<?= $urlPage ?>assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= $urlPage ?>assets/css/light.css">
   <link rel="stylesheet" href="<?= $urlPage ?>assets/css/normalize.css">
   <link rel="stylesheet" href="<?= $urlPage ?>assets/css/main.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
